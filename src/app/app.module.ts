da60d2186b71c4457aa206abaad86666b74bafe3
dcaceres <dcaceres@asjservicios.com>
2023-03-19 23:38:50 -0300

    Layout general ( primer desafio )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ToolbarComponent} from './toolbar/toolbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SidebarComponent,
+    HomeComponent,
+    FooterComponent
+
   ],
   imports: [
     BrowserModule
